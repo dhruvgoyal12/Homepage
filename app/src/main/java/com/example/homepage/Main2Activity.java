b6ead67f20c476510d128fae1b4b5df57b9a3212
Dhruv goyal <dhruvgoyalawesome@gmail.com>
2019-05-17 22:32:26 +0530

    new project on homepage and list

---
+++ app/src/main/java/com/example/homepage/Main2Activity.java
@@ -0,0 +1,53 @@
+package com.example.homepage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+    String[] NAMES={"Akash Sharma", "Bhanu" , "Brijraj" , "Charu" , "Dhruv Goyal" , "Harsh Chaudhary" ,"Kshitiz Kamal", "Muskan Garg"
+    ,"Nikita Rautela" , "Pravesh Sandhu" , "Rajat Sharma" , "Saransh " , "Sneha Yadav" ,"Vaibhav"} ;
+    String[] BRANCH={"CSE" , "ME" ,"ME" ,"CHE", "CSE" , "ME" , "CSE" , "CE" , "ME" , "CSE" , "EE" , "CHE" ,"EE" ,"CSE"} ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        ListView listview = findViewById(R.id.listview) ;
+
+        CustomAdapter customAdapter = new CustomAdapter();
+
+        listview.setAdapter(customAdapter);
+    }
+    class CustomAdapter extends BaseAdapter{
+        @Override
+        public int getCount() {
+            return NAMES.length;
+        }
+        @Override
+        public Object getItem(int i){
+            return null ;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i , View view , ViewGroup viewgroup){
+            view = getLayoutInflater().inflate(R.layout.customlayout,null) ;
+
+            TextView textView_name = view.findViewById(R.id.textView_name) ;
+            TextView textView_branch = view.findViewById(R.id.textView_branch);
+            textView_name.setText(NAMES[i]);
+            textView_branch.setText(BRANCH[i]) ;
+            return view ;
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/homepage/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.homepage;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.net.UrlQuerySanitizer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.net.URI;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    private Button bt3 ;
+    private Button bt2;
+    private Button bt1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt3 = findViewById(R.id.bt3) ;
+        bt2 = findViewById(R.id.bt2) ;
+        bt1 = findViewById(R.id.bt1) ;
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+        bt3.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                openWebsite("https://www.google.com");
+            }
+        });
+        bt1.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+            }
+        } );
+    }
+    public void openWebsite(String url){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url)) ;
+
+        startActivity(intent) ;
+
+    }
+    public void openActivity2(){
+        Intent intent = new Intent(MainActivity.this,Main2Activity.class) ;
+        startActivity(intent);
+    }
+    public void openActivity3(){
+        Intent intent = new Intent(MainActivity.this,Main3Activity.class) ;
+        startActivity(intent) ;
+    }
+}
